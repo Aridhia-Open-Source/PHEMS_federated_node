141ea2d1a83dd5873df35de619e61c0047e4afec
r-casula <riccardo.casula@aridhia.com>
2024-08-16 16:10:09 +0100

    argocd-installation: Added folder to handle argoCD installation. Added a doc for it

---
+++ ARGOCD.md
@@ -0,0 +1,29 @@
+# ArgoCD Installation
+
+We do reccommend to install ArgoCD before installing the federated node to keep up-to-date with little effort.
+
+The installation needs to be performed with some configuration tweaks, this is why the folder [install_argocd](./install_argocd/) exists.
+
+Within, the [install_argocd.sh](./install_argocd/install_argocd.sh) was created to automate the whole process.
+
+### Pre-requisite
+The github credentials will be needed (up to v1.0 or until it is decided to turn the repository open to all) to add the github repository as a source of truth.
+
+```sh
+./install_argocd/install_argocd.sh -u username -t token
+```
+
+### What does the installation script does
+- Create the `argocd` namespace
+- Add the helm repo locally
+- Install argo in the cluster via the helm chart
+- Add the Federated Node repository to ArgoCD
+
+### Install The Federated Node
+It can be done via CLI:
+```sh
+kubectl apply -f ./install_argocd/fn_application.yaml
+```
+In this case make sure the values file exists in the install_argocd folder or to paste the contents in the `spec.source.helm.values` section. Follow the comments and delete the values sections that are not used.
+
+Or via the UI, which is straightfotward.
---
+++ install_argocd/install_argocd.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+this=$(basename "$0")
+
+show_help() {
+    echo "$this [-u username] [-t token]"
+}
+while getopts "h?u:t:" opt; do
+  case "$opt" in
+    h|\?)
+      show_help
+      exit 0
+      ;;
+    u) username="$OPTARG"
+      ;;
+    t) password="$OPTARG"
+      ;;
+  esac
+done
+
+if [[ -z "$username" || -z "$password" ]]; then
+    echo "Missing credentials. Run $this -h to check how to provide these options"
+    exit 1
+fi
+echo "Installing ArgoCD"
+kubectl create namespace argocd
+
+# Via Helm chart
+helm repo add argo https://argoproj.github.io/argo-helm
+kubectl apply -f argo-extra.yaml
+helm install argocd argo/argo-cd -n argocd --create-namespace -f lookup-values.yaml
+
+# Install CLI - Optional
+echo "Installing ArgoCD CLI"
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+# Add Repo from github
+for repo in "https://github.com/Arihdia-Federated-Node/federated_node" "https://github.com/Arihdia-Federated-Node/go-controller"
+do
+    argocd repo add "${repo}" --username "${username}" --password "${password}"
+done
+
+# Add app via k8s manifest
+# kubectl apply -f fn_application.yaml
