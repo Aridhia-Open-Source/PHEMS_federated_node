1a4154d5641a67c83509b17718233f62a6ef019e
r-casula <riccardo.casula@aridhia.com>
2024-11-19 08:53:21 +0000

    certificate_update: Removed secret copying, is not necessary. Removed nginx restart

--- build/certs/scripts/apply_secret.sh
+++ build/certs/scripts/apply_secret.sh
@@ -7,7 +7,4 @@ chmod 644 privkey.pem fullchain.pem
 
 kubectl delete secret "${SSL_SECRET_NAME}" --ignore-not-found
 kubectl create secret -n "${NAMESPACE}" tls "${SSL_SECRET_NAME}" --key privkey.pem --cert fullchain.pem
-kubectl rollout restart deployment ingress-nginx-controller -n "${NGINX_NAMESPACE}"
 rm privkey.pem fullchain.pem
-
-kubectl get secret "${SSL_SECRET_NAME}" -n "${NAMESPACE}" -o yaml | sed '/namespace\|creationTimestamp\|resourceVersion\|uid:/d;' | kubectl apply -n "${KEYCLOAK_NAMESPACE}" -f -
