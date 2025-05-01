65c81bd06c1d5fa0f27a044b96555b90908b9254
r-casula <riccardo.casula@aridhia.com>
2025-05-01 16:28:46 +0100

    kc-credential-reset: Added service account to the keycloak resetter. Added summary to the pipeline

---
+++ build/alpine/keycloak-reset.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+psql -d "fn_${PGHOST}" -U "$PGUSER" -f - <<SQL
+    DELETE FROM credential WHERE user_id IN (SELECT user_id FROM user_entity WHERE username = 'admin');
+    DELETE FROM user_role_mapping WHERE user_id IN (SELECT user_id FROM user_entity WHERE username = 'admin');
+    DELETE FROM user_entity WHERE username = 'admin';
+SQL
+kubectl delete pods -n "${KC_NAMESPACE}" -l app=keycloak
