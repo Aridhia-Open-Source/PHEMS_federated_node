05836d38258e6a68a2e65ec249c2272691628cd4
r-casula <riccardo.casula@aridhia.com>
2025-05-02 08:17:42 +0100

    kc-credential-reset: Wrong configmap mounted for reset job

--- build/alpine/keycloak-reset.sh
+++ build/alpine/keycloak-reset.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-psql -d "fn_${PGDATABASE}" -U "$PGUSER" -f - <<SQL
+set -e
+
+echo "DB info:"
+echo "Host: ${PGHOST}"
+echo "User: ${PGUSER}"
+echo "DB: fn_${PGDATABASE}"
+
+psql -d "fn_${PGDATABASE}" -h "${PGHOST}" -U "${PGUSER}" -f - <<SQL
     DELETE FROM credential WHERE user_id IN (SELECT user_id FROM user_entity WHERE username = 'admin');
     DELETE FROM user_role_mapping WHERE user_id IN (SELECT user_id FROM user_entity WHERE username = 'admin');
     DELETE FROM user_entity WHERE username = 'admin';
