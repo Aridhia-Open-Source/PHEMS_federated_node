a0c2c859828603bd5f2b1f612735879250376584
Riccardo Casula <riccardo.casula@aridhia.com>
2025-05-06 11:55:45 +0100

    Update keycloak-reset.sh

--- build/alpine/keycloak-reset.sh
+++ build/alpine/keycloak-reset.sh
@@ -8,7 +8,7 @@ echo "User: ${PGUSER}"
 echo "DB: fn_${PGDATABASE}"
 
 psql -d "fn_${PGDATABASE}" -h "${PGHOST}" -U "${PGUSER}" -f - <<SQL
-    DELETE FROM credential WHERE user_id IN (SELECT user_id FROM user_entity WHERE username = 'admin');
-    DELETE FROM user_role_mapping WHERE user_id IN (SELECT user_id FROM user_entity WHERE username = 'admin');
+    DELETE FROM credential WHERE user_id IN (SELECT id FROM user_entity WHERE username = 'admin');
+    DELETE FROM user_role_mapping WHERE user_id IN (SELECT id FROM user_entity WHERE username = 'admin');
     DELETE FROM user_entity WHERE username = 'admin';
 SQL
