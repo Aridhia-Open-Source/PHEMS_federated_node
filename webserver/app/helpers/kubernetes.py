fc06944536e353ca881109647b2dfbff65fed0a9
Riccardo Casula <riccardocasula@aridhia.net>
2026-02-02 12:52:36 +0000

    139-k8s-error-handling: Replaced alpine image from task init container with the one we're creating

--- webserver/app/helpers/kubernetes.py
+++ webserver/app/helpers/kubernetes.py
@@ -9,7 +9,7 @@ from kubernetes.stream import stream
 from kubernetes.client.exceptions import ApiException
 from kubernetes.watch import Watch
 from app.helpers.exceptions import InvalidRequest, KubernetesException
-from app.helpers.const import TASK_NAMESPACE
+from app.helpers.const import ALPINE_IMAGE, TASK_NAMESPACE
 
 logger = logging.getLogger('kubernetes_helper')
 logger.setLevel(logging.INFO)
@@ -69,7 +69,7 @@ class KubernetesBase:
             ))
         container = client.V1Container(
             name=pod_spec["name"],
-            image="alpine:3.19",
+            image=ALPINE_IMAGE,
             volume_mounts=vol_mounts,
             command=["/bin/sh", "-c", f"sleep {60*60*24}"]
         )
