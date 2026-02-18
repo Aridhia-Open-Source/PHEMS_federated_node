4ed837ca5dd92d1d294c819b555d17630c107f40
codepint <george.eddie@aridhia.com>
2026-02-18 18:53:38 +0000

    working commit, basic dummy run sensor with artifact logging based on task_id

--- scripts/run_job.sh
+++ scripts/run_job.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 JOB_NAME="k8s_pipes_job"
-IMAGE="localhost:5001/julia-fn:v30"
+DOCKER_IMAGE="localhost:5001/julia-fn:v30"
 DAGSTER_GRAPHQL_API="http://localhost:3000/graphql"
 
 curl -X POST $DAGSTER_GRAPHQL_API \
   -H "Content-Type: application/json" \
-  -d "{\"query\":\"mutation { launchPipelineExecution(executionParams: { selector: { jobName: \\\"$JOB_NAME\\\", repositoryName: \\\"__repository__\\\", repositoryLocationName: \\\"dagster-fn\\\" }, runConfigData: { ops: { k8s_pipes_op: { config: { image: \\\"$IMAGE\\\" } } } } }) { __typename ... on LaunchPipelineRunSuccess { run { runId } } ... on PythonError { message } } }\"}"
+  -d "{\"query\":\"mutation { launchPipelineExecution(executionParams: { selector: { jobName: \\\"$JOB_NAME\\\", repositoryName: \\\"__repository__\\\", repositoryLocationName: \\\"dagster-fn\\\" }, runConfigData: { ops: { k8s_pipes_op: { config: { docker_image: \\\"$DOCKER_IMAGE\\\" } } } } }) { __typename ... on LaunchPipelineRunSuccess { run { runId } } ... on PythonError { message } } }\"}"
 
 echo
