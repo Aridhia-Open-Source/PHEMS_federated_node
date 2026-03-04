a089521bc3696cd486a58a7f00c8c9bf645f1542
codepint <george.eddie@aridhia.com>
2026-03-04 12:42:44 +0000

    Adding sensor to add a comment to the github PR after result upload completed

--- dagster/app/definitions/__init__.py
+++ dagster/app/definitions/__init__.py
@@ -2,12 +2,13 @@ import dagster as dg
 from dagster_k8s import PipesK8sClient
 
 from app.definitions.jobs import jobs
-from app.definitions.sensors import sensors, github_transfer_job
+from app.definitions.sensors import sensors
+from app.definitions.sensors import github_transfer_job, github_pr_comment_job
 
 pipes_k8s_client = PipesK8sClient(poll_interval=1.0)
 
 defs = dg.Definitions(
     sensors=sensors,
-    jobs=[*jobs, github_transfer_job],
+    jobs=[*jobs, github_transfer_job, github_pr_comment_job],
     resources={"k8s_pipes_client": pipes_k8s_client},
 )
