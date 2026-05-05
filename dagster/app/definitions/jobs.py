029502ba1d15f24d9671123de162f3f4d303f725
codepint <george.eddie@aridhia.com>
2026-05-05 15:38:09 +0100

    removing unused examples and tidying up project

--- dagster/app/definitions/jobs.py
+++ dagster/app/definitions/jobs.py
@@ -1,24 +1,12 @@
 import dagster as dg
 
-from app.definitions.examples import (
-    example_a_op,
-    example_b_op,
-    example_complex_op,
-    example_logging_op,
-)
+from app.definitions.examples import example_op
 from app.definitions.pipes import k8s_pipes_op
 
 
 @dg.job
-def example_complex_job():
-    a = example_a_op()
-    b = example_b_op()
-    example_complex_op(a, b)
-
-
-@dg.job
-def example_logging_job():
-    example_logging_op()
+def example_job():
+    example_op(5)
 
 
 @dg.job
@@ -27,7 +15,6 @@ def k8s_pipes_job():
 
 
 jobs = [
-    example_complex_job,
-    example_logging_job,
+    example_job,
     k8s_pipes_job,
 ]
