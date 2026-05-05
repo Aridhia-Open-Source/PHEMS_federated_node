3a774b719183dadea789d8d845236063836884dc
codepint <george.eddie@aridhia.com>
2026-05-05 15:11:56 +0100

    removing development models from project and moving them to local .models dir

--- scripts/build_all_images.sh
+++ scripts/build_all_images.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
+
+DOCKER_TAG=$1
 
 DOCKER_DIRS=(
   "dagster"
   "webserver"
   "github_transfer"
-  "docker_models/julia"
   "build/alpine"
+  ".models/julia"
 )
 
-DOCKER_TAG=$1
 
 for dir in "${DOCKER_DIRS[@]}"; do
+  if [[ ! -d "$dir" ]]; then
+    echo "Skipping '$dir': directory does not exist"
+    continue
+  fi
   ./scripts/build_image.sh "$dir" "$DOCKER_TAG"
 done
-
 
 echo "Image builds successful!"
