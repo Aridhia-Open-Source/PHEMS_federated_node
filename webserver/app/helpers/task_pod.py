fa284513098c0196e0396c26deecf6d4cdc19003
Liam Smith <liamsmith@aridhia.net>
2026-05-21 07:45:46 +0100

    Add support for GCP DNS-01 and Filestore

--- webserver/app/helpers/task_pod.py
+++ webserver/app/helpers/task_pod.py
@@ -124,6 +124,15 @@ class TaskPod:
                 path=os.getenv("NFS_PATH"),
                 read_only=False
             )
+        elif os.getenv("GCP_STORAGE_ENABLED"):
+            pv_spec.csi=V1CSIPersistentVolumeSource(
+                driver="filestore.csi.storage.gke.io",
+                volume_handle=os.getenv("GCP_VOLUME_HANDLE"),
+                volume_attributes={
+                    "ip": os.getenv("GCP_FILESTORE_IP"),
+                    "volume": os.getenv("GCP_SHARE_NAME")
+                }
+            )
         else:
             pv_spec.host_path=V1HostPathVolumeSource(
                 path=RESULTS_PATH
