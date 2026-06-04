871c96812fd9f5859a979c007b709afdb62c0e84
codepint <george.eddie@aridhia.com>
2026-06-04 12:51:16 +0100

    Refactoring README.md, updated README.md with hadolint and pip-compile sections. Updated run_hadolint.sh to output report logs, whilst propogating the exit code.

--- scripts/run_hadolint.sh
+++ scripts/run_hadolint.sh
@@ -5,22 +5,34 @@ set -e
 ARTIFACTS_DIR=artifacts
 
 if [ ! -x "$(which xmlstarlet)" ]; then
-  echo xmlstarlet not found, installing...
+  echo "xmlstarlet not found, installing..."
   sudo apt update
   sudo apt install xmlstarlet --no-install-recommends -y
 fi
 
-set +e
 #shellcheck disable=2046
-result=$(docker run \
+DOCKERFILES=$(find . -type f -name "Dockerfile")
+
+set +e
+
+# Run with readable output
+docker run \
   --volume "$(pwd)":/mnt:ro \
   --workdir /mnt \
   --init \
   --rm \
-  hadolint/hadolint:latest-alpine hadolint -f checkstyle $(find . -type f -name "Dockerfile"))
+  hadolint/hadolint:latest-alpine hadolint $DOCKERFILES
 exit_status=$?
+
+# Generate JUnit XML artifact for CI reporting
+docker run \
+  --volume "$(pwd)":/mnt:ro \
+  --workdir /mnt \
+  --init \
+  --rm \
+  hadolint/hadolint:latest-alpine hadolint -f checkstyle $DOCKERFILES \
+  | xmlstarlet tr scripts/checkstyle2junit.xslt > "$ARTIFACTS_DIR"/hadolint.xml
+
 set -e
-echo "$result" \
-  | xmlstarlet tr scripts/checkstyle2junit.xslt \
-  > "$ARTIFACTS_DIR"/hadolint.xml
+
 exit "$exit_status"
